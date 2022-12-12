b6c99c2d87d990eb12ead96fbbc15c61f3299859
이원욱 <cresend01@naver.com>
2022-12-12 17:25:48 +0900

    학번 유효성 검사

--- js/index.js
+++ js/index.js
@@ -37,9 +37,18 @@ userNameElement.onclick = () => {
 
 StudentID.onclick = () => {
     const sID = prompt("학번을 입력해주세요.");
-    // if (!(sID <= 9 && sID >= 0 )) {
-
-    // }s
+    if (sID.length != 9) {
+        confirm("잘못된 학번을 입력하였습니다. 다시 입력해주세요.");
+        return false;
+    }
+    for (i=0; i<sID.length; i++) {
+        var ch = sID.charAt(i);
+        if (!(ch >= '0' && ch <= '9')) {
+            confirm("학번은 숫자로만 입력 가능합니다.");
+            return false;
+        }
+    }
+    
     const regexp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
     const eMail = prompt("이메일을 입력해주세요.");
     if (eMail.match(regexp) === null) {
